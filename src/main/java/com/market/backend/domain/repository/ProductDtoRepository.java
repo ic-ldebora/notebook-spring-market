9ede467e1069bf4bcb97001479cad2dd21397e1a
ic-ldebora <ic-ldebora@github.com>
2021-01-26 01:43:32 -0600

    Added the product DTO repository.

---
+++ src/main/java/com/market/backend/domain/repository/ProductDtoRepository.java
@@ -0,0 +1,21 @@
+package com.market.backend.domain.repository;
+
+import com.market.backend.domain.ProductDto;
+
+import java.util.List;
+import java.util.Optional;
+
+public interface ProductDtoRepository {
+
+  List<ProductDto> getAll();
+
+  Optional<List<ProductDto>> getByCategory(Long categoryId);
+
+  Optional<List<ProductDto>> getScarseProducts(Integer quantity);
+
+  Optional<ProductDto> getProduct(Long id);
+
+  ProductDto save(ProductDto productDto);
+
+  void delete(Long id);
+}
