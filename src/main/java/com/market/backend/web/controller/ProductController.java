4b192ca1f9a9b4004362b02f88facd57c7142ac9
ic-ldebora <ic-ldebora@github.com>
2021-01-28 03:11:45 -0600

    Published the product endpoints.

--- src/main/java/com/market/backend/web/controller/ProductController.java
+++ src/main/java/com/market/backend/web/controller/ProductController.java
@@ -3,6 +3,11 @@ package com.market.backend.web.controller;
 import com.market.backend.domain.ProductDto;
 import com.market.backend.domain.service.ProductService;
 import org.springframework.beans.factory.annotation.Autowired;
+import org.springframework.web.bind.annotation.DeleteMapping;
+import org.springframework.web.bind.annotation.GetMapping;
+import org.springframework.web.bind.annotation.PathVariable;
+import org.springframework.web.bind.annotation.PostMapping;
+import org.springframework.web.bind.annotation.RequestBody;
 import org.springframework.web.bind.annotation.RequestMapping;
 import org.springframework.web.bind.annotation.RestController;
 
@@ -16,23 +21,28 @@ public class ProductController {
   @Autowired
   private ProductService productService;
 
+  @GetMapping("/all")
   public List<ProductDto> getAll() {
     return productService.getAll();
   }
 
-  public Optional<ProductDto> getProduct(Long id) {
+  @GetMapping("/{id}")
+  public Optional<ProductDto> getProduct(@PathVariable("id") Long id) {
     return productService.getProduct(id);
   }
 
-  public Optional<List<ProductDto>> getByCategory(Long categoryId) {
+  @GetMapping("/category/{id}")
+  public Optional<List<ProductDto>> getByCategory(@PathVariable("id") Long categoryId) {
     return productService.getByCategory(categoryId);
   }
 
-  public ProductDto save(ProductDto productDto) {
+  @PostMapping("/save")
+  public ProductDto save(@RequestBody ProductDto productDto) {
     return productService.save(productDto);
   }
 
-  public Boolean delete(Long id) {
+  @DeleteMapping("/delete/{id}")
+  public Boolean delete(@PathVariable("id") Long id) {
     return productService.delete(id);
   }
 }
