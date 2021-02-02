71e02a3b8d0fc402d79368a5d67f6efb6f770a88
ic-ldebora <ic-ldebora@github.com>
2021-02-02 00:09:51 -0600

    Added the generation of the token and the DTOs for the authentication request and response.

---
+++ src/main/java/com/market/backend/domain/dto/AuthenticationRequest.java
@@ -0,0 +1,12 @@
+package com.market.backend.domain.dto;
+
+import lombok.Data;
+import lombok.NoArgsConstructor;
+
+@Data
+@NoArgsConstructor
+public class AuthenticationRequest {
+
+  private String username;
+  private String password;
+}
---
+++ src/main/java/com/market/backend/web/security/JwtUtil.java
@@ -0,0 +1,25 @@
+package com.market.backend.web.security;
+
+import io.jsonwebtoken.Jwts;
+import io.jsonwebtoken.SignatureAlgorithm;
+import org.springframework.security.core.userdetails.UserDetails;
+
+import java.util.Date;
+
+public class JwtUtil {
+
+  // An expiration time of 10 hours as a sample.
+  private static final int EXPIRATION_TIME = 1000 * (60 * 60 * 10);
+
+  // A sample key.
+  private static final String KEY = "market";
+
+  public String genenrateToken(UserDetails userDetails) {
+    return Jwts.builder()
+        .setSubject(userDetails.getUsername())
+        .setIssuedAt(new Date())
+        .setExpiration(new Date(System.currentTimeMillis() + EXPIRATION_TIME))
+        .signWith(SignatureAlgorithm.HS256, KEY)
+        .compact();
+  }
+}
